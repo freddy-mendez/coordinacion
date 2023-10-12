40b9f3db9931c43bd80cfe1cdc3a124bc8eccc0f
freddy-mendez <freddy.mendez@misena.edu.co>
2023-10-12 11:24:02 -0500

    Carga de Datos

--- funciones.js
+++ funciones.js
@@ -1,32 +1,109 @@
 function mostrar_datos_aprendiz(aprendiz) {
-
+    var res = $("#resultado");
+    var tabla = $("<tabla></tabla>");
+    var thead = $("<thead><tr><th>Documento</th><th>Nombres</th><th>Email</th><th>Numero de ficha</th><th>Programa de formacion</th><th>Lider de la ficha</th></tr></thead>");
+    $(tabla).addClass("table");
+    $(tabla).append($("<h2>Datos del aprendiz</h2>"));
+    $(tabla).append(thead);
+    $(tabla).attr("id","id_tabla_aprendiz");
+    var tbody = $("<tbody></tbody");
+    var fila = $("<tr></tr>");
+    var celda = $("<td></td>");
+    celda.html(  aprendiz.documento  );
+    $(fila).append(celda);
+    celda = $("<td></td>");
+    celda.html(  aprendiz.nombres+" "+aprendiz.apellidos  );
+    $(fila).append(celda);
+    celda = $("<td></td>");
+    celda.html(  aprendiz.email  );
+    $(fila).append(celda);
+    celda = $("<td></td>");
+    celda.html(  aprendiz.numero  );
+    $(fila).append(celda);
+    celda = $("<td></td>");
+    celda.html(  aprendiz.programa  );
+    $(fila).append(celda);
+    celda = $("<td></td>");
+    celda.html(  aprendiz.lider  );
+    $(fila).append(celda);
+    $(fila).attr("id", "id_fila");
+    $(tbody).append(fila);
+    $(tabla).append(tbody);
+    $(res).append(tabla);
 }
 
 function mostrar_datos_historial(historial) {
+    var res = $("#resultado");
+    var tabla = $("<tabla></tabla>");
+    var thead = $("<thead><tr><th>Coordinador</th><th>Motivo</th><th>Fecha</th><th>Hora</th><th>Duracion</th></tr></thead>");        
+    $(tabla).addClass("table");
+    $(tabla).append($("<h2>Historial de salidas</h2>"));
+    $(tabla).append(thead);
+    $(tabla).attr("id","id_tabla_historial");
+    var tbody = $("<tbody></tbody>");
+    historial.forEach(element => {             
+        var fila = $("<tr></tr>");
+        var celda = $("<td></td>");
+        celda.html(  element.coordinador  );
+        $(fila).append(celda);
+        celda = $("<td></td>");
+        if (element.nombre_motivo=="Otros") {
+            celda.html(  element.otros  );
+        } else {
+            celda.html(  element.nombre_motivo  );
+        }      
+        $(fila).append(celda);
+        celda = $("<td></td>");
+        celda.html(  element.fecha  );
+        $(fila).append(celda);
+        celda = $("<td></td>");
+        celda.html(  element.hora  );
+        $(fila).append(celda);
+        celda = $("<td></td>");
+        celda.html(  element.duracion + " horas" );
+        $(fila).append(celda);
+        $(fila).addClass("id_fila_historial");
+        $(tbody).append(fila);      
+    });
+    $(tabla).append(tbody);
+    $(res).append(tabla);
     
 }
 
-$(document).ready(function() {
-    $("#btnBuscar").click(function(){
-        var documento = $("#documento").val();
+
+
+$(document).ready(function () {
+    $("#btn-buscar").click(function () {
+        var document = $("#documento").val();
+        var res = $("#resultado");
+        $(res).html("");
+        //$("#id_tabla_aprendiz").remove();
+        //$("#id_tabla_historial").remove();
         $.ajax({
-            url: "http://localhost/coordinacion/busqueda.php",
+            url:"/coordinacion/busqueda.php",
             type: "POST",
             dataType: "json",
             data: {
-                "documento":documento
+                "documento" : document
             },
-            success: function(data) {
+            success : function (data) {
                 if (data.estado=="OK") {
+                    console.log(data);
                     mostrar_datos_aprendiz(data.resultado.aprendiz);
                     mostrar_datos_historial(data.resultado.historial);
                 } else {
                     $("#resultado").text(data.msg);
                 }
+               
             },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.log(errorThrown);
+            error: function (jqXHR, textStatus, exception) {
+                console.log(exception);
             }
         });
     });
+
+    $("#btn-nueva-salida").click(function (){
+        var res = $("#resultado");
+        $(res).html("");
+    }); 
 });
--- home.php
+++ home.php
@@ -1,15 +1,17 @@
 <?php
-session_start();
 
-$id_user = $_SESSION['id_user'];
+    session_start();
 
-if (!$id_user) {
-    header('Location: index.php');
-    return;
-}
+    $id_user = $_SESSION["id_user"];
 
-$nombre = $_SESSION['nombre'];
+    if (!$id_user) {
+        header("Location: index.php");
+        return;
+    }
+
+    //$nombre = $_SESSION['nombre'];
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -17,40 +19,35 @@ $nombre = $_SESSION['nombre'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php
-    include('scripts.php');
-    ?>
+    <?php include ('scripts.php'); ?>
 </head>
 <body>
-    <div class="container">
-    <div class="row">
-            <div class="col">
+    <div class="container p-5">
+        <div class="row">
+        <div class="col">
             <a class="btn btn-danger float-end mt-2" href="logout.php">Logout</a>
             </div>
         </div>
-        <div class="row mt-5">
-            <div class="col">
-            <h1>Bienvenido <?php echo $nombre ?></h1>
-            </div>
-        </div>
         <div class="row">
-            <div class="col">
-            <form method="post" action="index.php">
-            <div class="input-group mb-3">
-                <input id="documento" type="number" class="form-control" placeholder="Ingrese Documento" >
-                <button class="btn btn-outline-secondary" type="button" id="btnBuscar" >Buscar</button>
-            </div>
+            <form action="index.php" method="post">
+                <input type="number" name="documento" id="documento">
+                <button type="button" id="btn-buscar" class="btn btn-success border">Buscar</button>
+                <button type="button" id="btn-nueva-salida" class="btn btn-dark">Nueva salida</button>
             </form>
+            
         </div>
+
+
         <div class="row">
             <div class="col">
                 <div id="resultado" class="resultado">
-
+                
                 </div>
             </div>
         </div>
-    </div>
-    </div>
-    
+
 </body>
 </html>
+
+
+
